ca382af194f0a85befc8ef4f24e94708af862c57
alexandre_poichet <apoichet@voyages-sncf.com>
2018-02-20 17:38:36 +0100

    feature: add bilan csv

--- src/main/java/com/ouitech/wdi/tfn/service/WriterResultsCsv.java
+++ src/main/java/com/ouitech/wdi/tfn/service/WriterResultsCsv.java
@@ -13,6 +13,7 @@ import static com.ouitech.wdi.tfn.MyProperties.getOutputResultsCsvPath;
 public class WriterResultsCsv implements WriterResults<Writer> {
 
     public static final String TITLE = "File Name;Test Suite;Test Case;Activate;Interfaces;Occurrence\n";
+    public static final String BILAN_TITLE = "\nNb TFN Actif;Nb TFN Inactif;TOTAL\n";
 
     @Override
     public Writer saveResults(List<Tfn> tfns) {
@@ -23,10 +24,27 @@ public class WriterResultsCsv implements WriterResults<Writer> {
             //Ecriture des titres
             writer.append(TITLE);
 
+            //Calcul Bilan
+            int total = 0;
+            int nbrInactive = 0;
+
             //Ecriture des resultats
             for (Tfn tfn : tfns) {
                 writer.append(printTfn(tfn));
+                total += tfn.nbrInterface();
+                if (tfn.isInactive()){
+                   nbrInactive += tfn.nbrInterface();
+                }
             }
+
+            //Bilan
+            writer.append(BILAN_TITLE);
+            writer.append(String.valueOf(total - nbrInactive))
+              .append(";")
+              .append(String.valueOf(nbrInactive))
+              .append(";")
+              .append(String.valueOf(total));
+
 
         } catch (IOException e) {
             e.printStackTrace();
